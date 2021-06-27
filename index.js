af90a8e5ddbe45c2ed0678ad13d0d768c44e4094
Lucas01PL <lucas01_ce@hotmail.com>
2021-06-27 13:55:53 -0300

    Ajuste deploy vercel.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-
+/*
 app.get('/', (req, res) => res.send('Home Page Route'));
 
 app.get('/about', (req, res) => res.send('About Page Route'));
@@ -9,6 +9,19 @@ app.get('/about', (req, res) => res.send('About Page Route'));
 app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
 app.get('/contact', (req, res) => res.send('Contact Page Route'));
+*/
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+app.post('/contribuicao', function (req, res) {
+  console.log(req.body);
+  res.send('hello world');
+});
 
 const port = process.env.PORT || 3000;
 
